c728f63045ab0f61dae982b1dac5df69d8dbbf51
AshenPheonix <brendlorend@gmail.com>
2019-07-11 13:50:24 -0700

    Stretch: Done

--- athlete/src/com/lambdaschool/solution/HockeyAthlete.java
+++ athlete/src/com/lambdaschool/solution/HockeyAthlete.java
@@ -1,8 +1,13 @@
 package com.lambdaschool.solution;
 
-public class HockeyAthlete implements Processor {
+public class HockeyAthlete implements Processor,CourtInjection {
     @Override
     public void displayAthlete() {
         System.out.printf("************%nHockey Goalie%n************%n");
+    }
+
+    @Override
+    public void displayCourt() {
+        System.out.printf("************%nHockey Rink%n************%n");
     }
 }
